64e4051386b7f23ff87ca9827dca9d5168a30695
Gon <gonzaa1694@gmail.com>
2024-02-06 20:46:14 -0300

    create controllers

---
+++ AmericaVirtualChallenge.Server/Controllers/GeocodingController.cs
@@ -0,0 +1,31 @@
+﻿using AmericaVirtualChallenge.Server.Services.Geocoding;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AmericaVirtualChallenge.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GeocodingController : ControllerBase
+    {
+        private readonly IGeocodingService _geocodingService;
+
+        public GeocodingController(IGeocodingService geocodingService)
+        {
+            _geocodingService = geocodingService;
+        }
+
+        [HttpGet("getCityCoordinates")]
+        public async Task<ActionResult<object>> GetCityCoordinates(string city)
+        {
+            try
+            {
+                var cityCoordinates = await _geocodingService.GetCityCoordinates(city);
+                return Ok(cityCoordinates);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Internal Server Error");
+            }
+        }
+    }
+}
---
+++ AmericaVirtualChallenge.Server/Controllers/WeatherController.cs
@@ -0,0 +1,37 @@
+﻿using AmericaVirtualChallenge.Server.Services.Geocoding;
+using AmericaVirtualChallenge.Server.Services.Weather;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AmericaVirtualChallenge.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherController : ControllerBase
+    {
+        private readonly IGeocodingService _geocodingService;
+        private readonly IWeatherService _weatherService;
+
+        public WeatherController(IGeocodingService geocodingService, IWeatherService weatherService)
+        {
+            _geocodingService = geocodingService;
+            _weatherService = weatherService;
+        }
+
+        [HttpGet("getWeatherByCity")]
+        public async Task<IActionResult> GetWeatherByCity(string city)
+        {
+            try
+            {
+                var cityCoordinates = await _geocodingService.GetCityCoordinates(city);
+
+                var weatherData = await _weatherService.GetWeatherByCoordinates(cityCoordinates.Lat, cityCoordinates.Lon);
+
+                return Ok(weatherData);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error: {ex.Message}");
+            }
+        }
+    }
+}
