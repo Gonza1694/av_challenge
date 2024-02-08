b8b6b23f0d650c3ce68dc4d1971ad0c4473bb60b
Gon <gonzaa1694@gmail.com>
2024-02-08 01:24:36 -0300

    add geocoding response model

---
+++ AmericaVirtualChallenge.Data/Models/GeocodingResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace AmericaVirtualChallenge.Data.Models
+{
+    public class GeocodingResponse
+    {
+        public string Name { get; set; }
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+        public string Country { get; set; }
+    }
+}
